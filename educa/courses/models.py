baff239d5f0c8baf2048499a703da81468f4b6d8
Prateek Lodhi <mailprateeklodhi@gmail.com>
2023-09-26 09:22:26 +0530

    educa model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    created = models.DateField()
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
